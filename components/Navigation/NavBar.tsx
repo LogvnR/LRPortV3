f8aa381f4fcbd1bd2f3ae7f7029fb0436b31998d
Logan Ricard <logvnjs@gmail.com>
2022-05-31 12:21:04 -0500

    added Framer Motion to NavBar

--- components/Navigation/NavBar.tsx
+++ components/Navigation/NavBar.tsx
@@ -1,20 +1,58 @@
 import { FC, useState } from 'react';
+import { motion } from 'framer-motion';
 
 import { Fade as Hamburger } from 'hamburger-react';
 import classes from '../../styles/Navigation/NavBar.module.css';
 
 const NavBar: FC = () => {
   const [menuToggle, setMenuToggle] = useState(false);
+
+  const variants = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: '-150%' },
+  };
+
   return (
     <nav className={classes.container}>
       <h4>lr</h4>
-      {menuToggle && (
-        <div className={classes.menu}>
-          <p>projects</p>
-          <p>contact</p>
-          <p>theme</p>
-        </div>
-      )}
+
+      <div className={classes.menu}>
+        <motion.p
+          initial="hidden"
+          variants={variants}
+          animate={menuToggle ? 'visible' : 'hidden'}
+          transition={{ type: 'spring', stiffness: 175, duration: 0.2 }}
+        >
+          projects
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          variants={variants}
+          animate={menuToggle ? 'visible' : 'hidden'}
+          transition={{
+            delay: 0.1,
+            type: 'spring',
+            stiffness: 175,
+            duration: 0.2,
+          }}
+        >
+          contact
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          variants={variants}
+          animate={menuToggle ? 'visible' : 'hidden'}
+          transition={{
+            delay: 0.2,
+            type: 'spring',
+            stiffness: 175,
+            duration: 0.2,
+          }}
+        >
+          theme
+        </motion.p>
+      </div>
+
       <div className={classes.hamburger}>
         <Hamburger
           toggled={menuToggle}
