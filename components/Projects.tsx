fa24f991cd3997c072cdb21f3fbc11711f0c2f42
Logan Ricard <logvnjs@gmail.com>
2022-10-09 15:30:48 -0500

    added responsive tailwind styles to main Projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { motion } from 'framer-motion';
 
-import classes from '../styles/Components/Projects.module.css';
 import ProjectCard from './Cards/ProjectCard';
 import Title from './UI/Title';
 
@@ -18,7 +17,7 @@ const Projects: FC = () => {
       <p className="mt-4 text-xs italic font-light tracking-wider font-Roboto text-dark-blue dark:text-light-grey">
         Projects are listed from latest to earliest
       </p>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center md:flex-row md:flex-wrap md:justify-center md:gap-y-8 lg:gap-x-8 lg:gap-y-16 xl:gap-y-12">
         {ProjectInfo.map((project) => (
           <ProjectCard {...project} key={project.key} />
         ))}
@@ -29,7 +28,7 @@ const Projects: FC = () => {
           href="https://github.com/LogvnR"
           role="button"
           target="_blank"
-          className="w-2/3 py-3 my-4 text-sm font-medium text-center uppercase transition-all duration-150 ease-in-out bg-transparent border border-solid text-dark-blue border-dark-blue dark:text-light-grey dark:border-light-grey hover:bg-black hover:text-white hover:border-black dark:hover:bg-white dark:hover:text-black dark:hover:border-white"
+          className="w-2/3 py-3 my-4 text-sm font-medium text-center uppercase transition-all duration-150 ease-in-out bg-transparent border border-solid text-dark-blue border-dark-blue dark:text-light-grey dark:border-light-grey hover:bg-black hover:text-white hover:border-black dark:hover:bg-white dark:hover:text-black dark:hover:border-white md:w-1/3 lg:w-1/6"
         >
           view more on github
         </motion.a>
