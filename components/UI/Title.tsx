d8e65f528499885a7fdca103236e0c993b9bfc7a
Logan Ricard <logvnjs@gmail.com>
2022-10-09 15:38:37 -0500

    added Title component tailwind styles

--- components/UI/Title.tsx
+++ components/UI/Title.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 
-import classes from '../../styles/UI/Title.module.css';
 import ScrollReveal from '../Cards/ScrollReveal';
 
 interface Props {
@@ -9,10 +8,12 @@ interface Props {
 
 const Title: FC<Props> = ({ title }) => {
   return (
-    <ScrollReveal className={classes.container}>
+    <ScrollReveal className="flex flex-col items-center w-full">
       <>
-        <h2>{title}</h2>
-        <div className={classes.border}></div>
+        <h2 className="my-[1px] text-center pb-1 text-xl font-Raleway uppercase font-normal text-dark-blue dark:text-light-grey">
+          {title}
+        </h2>
+        <div className="h-[2px] w-1/5 max-w-[90px] bg-gradient-to-r from-accent to-accent-alt"></div>
       </>
     </ScrollReveal>
   );
